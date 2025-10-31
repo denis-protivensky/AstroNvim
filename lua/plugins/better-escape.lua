6a205721b0a93c2e6211c41a97b38ec232f13eb5
Denis Protivensky <fordeso2@gmail.com>
2025-10-31 18:45:15 +0300

    Configure better-escape to use qq

---
+++ lua/plugins/better-escape.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  { "max397574/better-escape.nvim",
+    opts = {
+      mappings = {
+        i = {
+          q = {
+            q = "<Esc>",
+          },
+          j = {
+            j = false,
+            k = false,
+          },
+        },
+      },
+    }
+  },
+}
